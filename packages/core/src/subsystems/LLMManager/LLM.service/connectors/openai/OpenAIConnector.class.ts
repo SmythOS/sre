f52bd1d6f361b6df611c51d9d7cce83aa24ee1ca
Forhad Hosain <forhad@smythos.com>
2025-10-16 01:55:59 +0600

    validate token limit for legacy models only

--- packages/core/src/subsystems/LLMManager/LLM.service/connectors/openai/OpenAIConnector.class.ts
+++ packages/core/src/subsystems/LLMManager/LLM.service/connectors/openai/OpenAIConnector.class.ts
@@ -83,17 +83,18 @@ export class OpenAIConnector extends LLMConnector {
             logger.debug(`request ${this.name}`, acRequest.candidate);
             const _body = body as OpenAI.ChatCompletionCreateParams;
 
-            // #region Validate token limit
-            const messages = _body?.messages || [];
-            const lastMessage = messages[messages.length - 1];
-            const promptTokens = await this.computePromptTokens(messages, context);
+            // #region Validate the token limit only if it's a legacy model.
+            if (context?.modelEntryName?.startsWith('legacy/')) {
+                const messages = _body?.messages || [];
+                const promptTokens = await this.computePromptTokens(messages, context);
 
-            await this.validateTokenLimit({
-                acRequest,
-                promptTokens,
-                context,
-                maxTokens: _body.max_completion_tokens,
-            });
+                await this.validateTokenLimit({
+                    acRequest,
+                    promptTokens,
+                    context,
+                    maxTokens: _body.max_completion_tokens,
+                });
+            }
             // #endregion Validate token limit
 
             const responseInterface = this.getInterfaceType(context);
@@ -146,17 +147,19 @@ export class OpenAIConnector extends LLMConnector {
     protected async streamRequest({ acRequest, body, context }: ILLMRequestFuncParams): Promise<EventEmitter> {
         try {
             logger.debug(`streamRequest ${this.name}`, acRequest.candidate);
-            // #region Validate token limit
-            const messages = body?.messages || body?.input || [];
-            const lastMessage = messages[messages.length - 1];
-            const promptTokens = await this.computePromptTokens(messages, context);
 
-            await this.validateTokenLimit({
-                acRequest,
-                promptTokens,
-                context,
-                maxTokens: body.max_completion_tokens,
-            });
+            // #region Validate the token limit only if it's a legacy model.
+            if (context?.modelEntryName?.startsWith('legacy/')) {
+                const messages = body?.messages || body?.input || [];
+                const promptTokens = await this.computePromptTokens(messages, context);
+
+                await this.validateTokenLimit({
+                    acRequest,
+                    promptTokens,
+                    context,
+                    maxTokens: body.max_completion_tokens,
+                });
+            }
             // #endregion Validate token limit
 
             const responseInterface = this.getInterfaceType(context);
