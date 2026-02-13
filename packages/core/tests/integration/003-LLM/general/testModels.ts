cb3f73267cb6f88aecfebf2b4f089d272f524146
Forhad Hosain <forhad@smythos.com>
2026-02-14 03:36:17 +0600

    restore testModels.ts to exclude new changes from this PR

--- packages/core/tests/integration/003-LLM/general/testModels.ts
+++ packages/core/tests/integration/003-LLM/general/testModels.ts
@@ -2,36 +2,6 @@ const testModels = [
     { provider: 'OpenAI', id: 'gpt-4o-mini', features: ['multiple-tools'] },
     { provider: 'Anthropic', id: 'claude-3.5-haiku' },
     { provider: 'GoogleAI', id: 'gemini-1.5-flash' },
-    // * LM Studio must be running with the following local model installed for these tests to pass.
-    // {
-    //     provider: 'OpenAI',
-    //     id: {
-    //         provider: 'OpenAI',
-    //         modelId: 'qwen2.5-7b-instruct-1m',
-    //         baseURL: 'http://localhost:1234/v1',
-    //         tokens: 8192,
-    //         completionTokens: 4096,
-    //         enabled: true,
-    //         features: ['text', 'tools'],
-    //         credentials: 'vault',
-    //         isCustomLLM: true,
-    //     },
-    // },
-    // * Ollama must be running with the following local model installed for these tests to pass.
-    // {
-    //     provider: 'Ollama',
-    //     id: {
-    //         provider: 'Ollama',
-    //         modelId: 'llama3.1:latest',
-    //         baseURL: 'http://127.0.0.1:11434/',
-    //         tokens: 8192,
-    //         completionTokens: 4096,
-    //         enabled: true,
-    //         features: ['text', 'tools'],
-    //         credentials: 'vault',
-    //         isCustomLLM: true,
-    //     },
-    // },
     //{ provider: 'Groq', id: 'gemma2-9b-it' },
     //{ provider: 'TogetherAI', id: 'meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo' },
     // { provider: 'TogetherAI', id: 'meta-llama/Meta-Llama-3-8B-Instruct-Lite' },
