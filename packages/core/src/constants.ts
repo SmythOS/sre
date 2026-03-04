b3462078e85c561d4305d09b5a96cf2d95ecb10b
Forhad Hosain <forhad@smythos.com>
2026-03-04 08:41:23 +0600

    increase MAX_FILE_SIZE

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -28,7 +28,7 @@ export const MAX_DEPTH = 10;
 export const MAX_OBJECT_SIZE = 1000;
 export const MAX_ARRAY_SIZE = 1000;
 
-export const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB
+export const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
 
 export const MAX_FILE_COUNT = 10;
 
